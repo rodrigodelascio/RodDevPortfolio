bb1f7c54646e2fde82437a0e52fe628f295d9213
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2022-12-02 15:58:01 +0000

    Media query adjustments

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const scrollIcon = document.getElementById("scrollIcon")
-
+const scrollIcon = document.getElementById("scrollIcon");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.getElementById("myNavLinks");
+const navLink = document.querySelectorAll(".myLink");
 
 // Scroll icon to hide
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 100) {
-        scrollIcon.style.visibility = "hidden"
-    } else {
-        scrollIcon.style.visibility = "visible"
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    scrollIcon.style.visibility = "hidden";
+  } else {
+    scrollIcon.style.visibility = "visible";
+  }
+});
 
+// Burger menu
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navLink.forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
